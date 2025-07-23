8a505abf477b7ea1dec6ac8a5037d303bfe3acd7
Dunamis <callmestiless@gmail.com>
2025-07-23 11:40:01 +0300

    add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import { navLinks } from "../constants/nav-links";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="content-container pt-10 flex flex-row items-center justify-between text-white font-gilroy-medium">
+        <a href="/" aria-label="homepage">
+          <h3 className="font-chronicle text-3xl font-semibold">MNTN</h3>
+        </a>
+
+        <ul className="flex flex-row items-center gap-10">
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={link.href}>{link.label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/constants/nav-links.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    id: 1,
+    label: "Equipment",
+    href: "#equipment",
+  },
+  {
+    id: 2,
+    label: "About Us",
+    href: "#about",
+  },
+  {
+    id: 3,
+    label: "Contact",
+    href: "#contact",
+  },
+  {
+    id: 4,
+    label: "Blog",
+    href: "#blog",
+  },
+];
